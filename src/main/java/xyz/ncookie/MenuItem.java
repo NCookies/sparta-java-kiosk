9349a53c29c428d110c234b5fd6ca8c8e0dcbcf2
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-07 14:59:01 +0900

    #3 - 햄버거 메뉴를 클래스로 관리하기
    - 햄버거 메뉴를 `MenuItem` 클래스와 `List`를 통해 관리한다.
    - `MenuItem` 클래스를 활용하여 햄버거 메뉴를 출력한다.

---
+++ src/main/java/xyz/ncookie/MenuItem.java
@@ -0,0 +1,13 @@
+package xyz.ncookie;
+
+public class MenuItem {
+    String name;
+    double price;
+    String desc;
+
+    public MenuItem(String name, double price, String desc) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+    }
+}
