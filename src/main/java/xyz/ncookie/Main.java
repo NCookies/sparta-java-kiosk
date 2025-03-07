75fe9109dab36942bf5f647456affd1ffbdc3405
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-07 14:42:55 +0900

    #2 - 기본적인 키오스크 구현
    - 햄버거 메뉴를 출력한다.
    - 제시된 메뉴 중 입력받은 숫자에 따라 다른 로직을 실행한다.
    - 특정 번호가 입력되면 프로그램을 종료한다.

--- src/main/java/xyz/ncookie/Main.java
+++ src/main/java/xyz/ncookie/Main.java
@@ -1,7 +1,33 @@
 package xyz.ncookie;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Kiosk!");
+        Scanner sc = new Scanner(System.in);
+        int input = -1;
+        
+        // 마지막에 "0"이 입력되기 전까지 루프
+        while (input != 0) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료      | 종료");
+
+            input = sc.nextInt();
+
+            switch (input) {
+                case 1 -> System.out.println("ShackBurger 구매!");
+                case 2 -> System.out.println("SmokeShack 구매!");
+                case 3 -> System.out.println("Cheeseburger 구매!");
+                case 4 -> System.out.println("Hamburger 구매!");
+                case 0 -> System.out.println("종료!");
+                default -> System.out.println("올바르지 않은 입력입니다!");
+            }
+        }
+
+        System.out.println("프로그램을 종료합니다.");
     }
 }
